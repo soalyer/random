d9e21a34f6a33da04a8f79007afcaa7c5b777f94
soalyer <94017270+soalyer@users.noreply.github.com>
2024-02-02 22:33:04 -0500

    Add files via upload

--- index-script.js
+++ index-script.js
@@ -11,12 +11,12 @@ websites = [
     new Website("Mandelbrot", "https://soalyer.github.io/random/fractal", "fractal.png"),
     new Website("Frog game", "https://soalyer.itch.io/leaps-and-bounds?secret=QMBpR3IrWqOHnE8nMri9TtPPbUw", "frog-game.png"),
     new Website("Find the emoji", "https://soalyer.github.io/random/emoji", "emoji.png"),
+    new Website("Sorting", "https://soalyer.github.io/random/sorting", "sorting.png"),
     new Website("Search algorithm", "https://soalyer.github.io/random/search", "search.png"),
     new Website("Color guesser", "https://soalyer.github.io/random/colours", "colours.png"),
     new Website("Maze generator", "https://soalyer.github.io/random/maze", "maze.png"),
     new Website("Icosahedron", "http://soalyer.github.io/random/icosahedron", "icosahedron.png"),
     new Website("Buffon's needle", "https://soalyer.github.io/random/buffon", "buffon.png"),
-    new Website("Bogosort", "https://soalyer.github.io/random/bogo", "bogo.png"),
     new Website("Emoji game", "https://soalyer.itch.io/silly-emoji-game?secret=PhUXSwYwQofBvJhedeu6Jlafp0o", "emoji-game.png")
 ];
 
